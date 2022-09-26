9f1ad1123299d2a0471a6dee94bf4e607a07899a
enumeration167 <112817653+enumeration167@users.noreply.github.com>
2022-09-26 23:28:46 +0100

    Add files via upload

---
+++ bypass the chat.lua
@@ -0,0 +1,149 @@
+local Bypasses = {
+    ["ass"] = "as{{aieixzvzx:s}}",
+    ["cumming"] = "cu{{aieixzvzx:mming}}",
+    ["i love dick"] = "i love di{{aieixzvzx:ck}}",
+    ["dick sucker"] = "di{{aieixzvzx:ck sucker}}",
+    ["asshole"] = "as{{aieixzvzx:shole}}",
+    ["bitch"] = "bit{{aieixzvzx:ch}}",
+    ["cock"] = "co{{aieixzvzx:ck}}",
+    ["cunt"] = "cu{{aieixzvzx:nt}}",
+    ["lmao"] = "l{{aieixzvzx:mao}}",
+    ["dick"] = "di{{zczxczvz:ck}}",
+    ["dyke"] = "dy{{aieixzvzx:ke}}",
+    ["faggot"] = "fa{{aieixzvzx:ggot}}",
+    ["negro"] = "ne{{aieixzvzx:gro}}",
+    ["pija"] = "pi{{aieixzvzx:ja}}",
+    ["chuparme"] = "ch{{aieixzvzx:uparme}}",
+    ["fuck"] = "fu{{aieixzvzx:ck}}",
+    ["gaylord"] = "gayl{{aieixzvzx:ord}}",
+    ["cum"] = "cu{{aieixzvzx:m}}",
+    ["sex"] = "se{{aieixzvzx:x}}",
+    ["hitler"] = "hit{{aieixzvzx:ler}}",
+    ["cum"] = "cu{{aieixzvzx:m}}",
+    ["kike"] = "ki{{aieixzvzx:ke}}",
+    ["motherfucker"] = "motherf{{aieixzvzx:ucker}}",
+    ["nigga"] = "ni{{aieixzvzx:gga}}",
+    ["nigger"] = "ni{{aieixzvzx:gger}}",
+    ["piss"] = "p{{aieixzvzx:iss}}",
+    ["penis"] = "pe{{aieixzvzx:nis}}",
+    ["pussy"] = "pu{{aieixzvzx:ssy}}",
+    ["shit"] = "sh{{aieixzvzx:it}}",
+    ["slut"] = "sl{{aieixzvzx:ut}}",
+    ["whore"] = "who{{aieixzvzx:re}}",
+    ["discord"] = "disco{{aieixzvzx:rd}}",
+    ["kys"] = "k{{aieixzvzx:ys}}",
+    ["slave"] = "sla{{aieixzvzx:ve}}",
+    ["niggers"] = "ni{{aieixzvzx:ggers}}",
+    ["date"] = "da{{aieixzvzx:te}}",
+    ["raped"] = "ra{{aieixzvzx:ped}}",
+    ["rape"] = "ra{{aieixzvzx:pe}}",
+    ["vape"] = "va{{aieixzvzx:pe}}",
+    ["wtf"] = "wt{{aieixzvzx:f}}",
+    ["hentai"] = "hen{{aieixzvzx:tai}}",
+    ["perra"] = "per{{aieixzvzx:ra}}",
+    ["mierda"] = "mie{{aieixzvzx:rda}}",
+    ["pene"] = "pe{{aieixzvzx:ne}}",
+    ["follar"] = "fol{{aieixzvzx:lar}}",
+    ["follamos"] = "fol{{aieixzvzx:lamos}}",
+    ["follemos"] = "fol{{aieixzvzx:lemos}}",
+    ["chupala"] = "ch{{aieixzvzx:upala}}",
+    ["puto"] = "pu{{aieixzvzx:to}}",
+    ["sexo"] = "se{{aieixzvzx:xo}}",
+    ["puta"] = "pu{{aieixzvzx:ta}}",
+    ["porno"] = "po{{aieixzvzx:rno}}",
+    ["porn"] = "po{{aieixzvzx:rn}}",
+    ["pornhub"] = "po{{aieixzvzx:rnhub}}",
+    ["xvideos"] = "xvi{{aieixzvzx:deos}}",
+    ["tits"] = "ti{{aieixzvzx:ts}}",
+    ["chupame"] = "ch{{aieixzvzx:upame}}",
+    ["discord.com"] = "disco{{aieixzvzx:rd.com}}",
+    ["coon"] = "co{{aieixzvzx:on}}",
+    ["retarded"] = "reta{{aieixzvzx:rded}}",
+    ["moonman"] = "moon{{aieixzvzx:man}}",
+    ["jews"] = "je{{aieixzvzx:ws}}",
+    ["nazis"] = "na{{aieixzvzx:zis}}",
+    ["faggots"] = "fa{{aieixzvzx:ggots}}",
+    ["masturbate"] = "mastu{{aieixzvzx:rbate}}",
+    ["femboy"] = "fem{{aieixzvzx:boy}}",
+    ["cums"] = "cu{{aieixzvzx:ms}}",
+    ["masturbating"] = "mastu{{aieixzvzx:rbating}}",
+    ["allah"] = "al{{aieixzvzx:lah}}",
+    ["akbar"] = "akb{{aieixzvzx:ar}}",
+    ["meth"] = "me{{aieixzvzx:th}}",
+    ["masturbated"] = "mastu{{aieixzvzx:rbated}}",
+    ["nazi"] = "na{{aieixzvzx:zi}}",
+    ["heil"] = "he{{aieixzvzx:il}}",
+    ["have"] = "ha{{aieixzvzx:ve}}",
+    ["masturbation"] = "mastu{{aieixzvzx:rbation}}",
+    ["rapist"] = "rap{{aieixzvzx:ist}}",
+    ["niggas"] = "ni{{aieixzvzx:ggas}}",
+    ["love"] = "lo{{aieixzvzx:ve}}",
+    ["black"] = "bl{{aieixzvzx:ack}}",
+    ["suck"] = "suc{{aieixzvzx:k}}",
+    ["beaner"] = "be{{aieixzvzx:aner}}",
+    ["beaners"] = "be{{aieixzvzx:aners}}",
+    ["spic"] = "sp{{aieixzvzx:ic}}",
+    ["spics"] = "sp{{aieixzvzx:ics}}",
+    ["mexico"] = "me{{aieixzvzx:xico}}",
+    ["mexican"] = "me{{aieixzvzx:xican}}",
+    ["mexicans"] = "me{{aieixzvzx:xicans}}",
+    ["child"] = "ch{{aieixzvzx:ild}}",
+    ["predator"] = "pred{{aieixzvzx:ator}}",
+    ["fags"] = "fa{{aieixzvzx:gs}}",
+    ["kikes"] = "ki{{aieixzvzx:kes}}",
+    ["dykes"] = "dy{{aieixzvzx:kes}}",
+    ["slaves"] = "sla{{aieixzvzx:ves}}",
+    ["enslave"] = "ensla{{aieixzvzx:ve}}",
+    ["autistic"] = "aut{{aieixzvzx:istic}}",
+    ["shut"] = "shu{{aieixzvzx:t}}",
+    ["the"] = "th{{aieixzvzx:e}}",
+    ["autism"] = "aut{{aieixzvzx:ism}}",
+    ["kill"] = "ki{{aieixzvzx:ll}}",
+    ["jihad"] = "ji{{aieixzvzx:had}}",
+    ["kkk"] = "k{{aieixzvzx:kk}}",
+    ["ku"] = "k{{aieixzvzx:u}}",
+    ["klux"] = "kl{{aieixzvzx:ux}}",
+    ["klan"] = "k{{aieixzvzx:lan}}",
+    ["george"] = "ge{{aieixzvzx:orge}}",
+    ["floyd"] = "fl{{aieixzvzx:oyd}}",
+    ["adolf"] = "ad{{aieixzvzx:olf}}",
+    ["arab"] = "ar{{aieixzvzx:ab}}",
+    ["arabs"] = "ar{{aieixzvzx:abs}}",
+
+}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remote = ReplicatedStorage:FindFirstChild("SayMessageRequest", true)
+
+local ChatBypass; ChatBypass = hookmetamethod(Remote, "__namecall", function(self, ...)
+    local Method = getnamecallmethod()
+    local Arguments = {...}
+    
+    if self == Remote and Method == "FireServer" then
+        local Message = Arguments[1]
+        local Split = Message:split(" ")
+        local FinalMessage = ""
+
+        for _, x in next, Split do
+            for _, Bypass in next, Bypasses do
+                if x:lower() == _ then
+                    if x:upper() ~= x then
+                        Message = Message:gsub(x, Bypass)
+                        FinalMessage = Message .. " ng"
+                    else
+                        Message = Message:gsub(x, Bypass):upper()
+                        FinalMessage = Message:gsub(x, Bypass):upper() .. " ng"
+                    end
+                end
+            end
+        end
+        
+        if FinalMessage ~= "" then
+            Arguments[1] = FinalMessage
+        end
+        
+        return ChatBypass(self, unpack(Arguments))
+    end
+    
+    return ChatBypass(self, ...)
+end)
